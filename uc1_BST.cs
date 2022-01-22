34c7578d2fe3e4a0f413e5a2c8ff0f496f0df7cb
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-22 20:39:03 +0530

    add values in binary search tree

---
+++ uc1_BST.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingAndBST
+{
+    public class uc1_BST<T> where T : IComparable<T>
+    {
+        public T nodeData { get; set; }
+        public uc1_BST<T> leftTree { get; set; }
+
+        public uc1_BST<T> rightTree { get; set; }
+
+        public uc1_BST(T data)
+        {
+            this.nodeData = data;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+
+        public void Insert(T item)
+        {
+            T CurrNodeVal = this.nodeData;
+            if ((CurrNodeVal.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new BST<T>(item);
+                }
+                else
+                {
+                    this.leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new BST<T>(item);
+                }
+                else
+                {
+                    this.rightTree.Insert(item);
+                }
+            }
+        }
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.nodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+
+    }
+}
